9d98a9e5f3826eee38af9f7e4a5d67076041bb0e
taeguk <xornrbboy@gmail.com>
2016-03-28 09:27:40 -0400

    rm requests and bs4. modify folder structure

--- github_listener/notification.py
+++ github_listener/notification.py
@@ -1,7 +1,7 @@
 #-*- coding: utf-8 -*-
 
-from .packages import requests
-from .packages.bs4 import BeautifulSoup
+from .github_listener import GithubAccount
+from .parser.notification_parser import NotificationParser
 
 class Notification(object):
     def __init__(self, title, link, persons):
@@ -45,58 +45,6 @@ class NotificationChange(object):
 
     def is_changed(self):
         return len(self.n_groups) >= 1
-
-class NotificationParser(object):
-    def __init__(self, account):
-        self.account  = account
-
-    def parse(self):
-        notification_groups = []
-
-        ID = self.account.username
-        PW = self.account.password
-
-        r = requests.get("https://github.com/login")
-
-        if r.status_code != 200:
-            raise Exception("Mistery Error")
-
-        soup = BeautifulSoup(r.text, 'html.parser')
-        CSRF_TOKEN = soup.find(attrs={"name": "authenticity_token"})['value']
-
-        r = requests.post("https://github.com/session",
-            data = {"commit" : "Sign in", "utf8" : "%E2%9C%93", "authenticity_token" : CSRF_TOKEN,
-                    "login" : ID, "password" : PW}, cookies=r.cookies)
-
-        if r.status_code != 200:
-            raise Exception("Login Error")
-
-        soup = BeautifulSoup(r.text, 'html.parser')
-        alert_box = soup.find(id='js-flash-container')
-        if str(alert_box).find("Incorrect") >= 0:
-            raise Exception("Login Fail")
-
-        r = requests.get("https://github.com/notifications", cookies=r.cookies)
-
-        if r.status_code != 200:
-            raise Exception("Loading Error")
-
-        soup = BeautifulSoup(r.text, 'html.parser')
-        target_area = soup.find_all(class_="notifications-list")[0]
-
-        raw_group_list = target_area.find_all(class_="js-notifications-browser", recursive=False)
-        for raw_group in raw_group_list:
-            group_name = raw_group.h3.a.get_text().strip()
-            raw_n_list = raw_group.ul.find_all('li', recursive=False)
-            notification_group = NotificationGroup(group_name)
-            notification_groups.append(notification_group)
-            for raw_n in raw_n_list:
-                n_title = raw_n.span.a.get_text().strip()
-                n_link = raw_n.span.a.get('href').strip()
-                n_persons = raw_n.ul.find_all(class_='tooltipped', recursive=False)[0].get('aria-label').strip()
-                notification_group.add_notification(Notification(n_title, n_link, n_persons))
-
-        return notification_groups
 
 class NotificationChecker(object):
     def __init__(self, account):
---
+++ github_listener/parser/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .notification_parser import NotificationParser
+
+__all__ = ['notification_parser']
