44fd5d3015455270076cfa59779a5a045b037519
taeguk <xornrbboy@gmail.com>
2016-03-29 13:54:34 +0900

    pypi version 0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup (
     name = 'github_listener',
-    version = '0.0.1',
+    version = '0.0.2',
 
     author = 'taeguk',
     author_email = 'xornrbboy@gmail.com',
