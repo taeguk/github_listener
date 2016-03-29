a55851c039043d6c114df887cb763b355feb4523
taeguk <xornrbboy@gmail.com>
2016-03-29 13:00:45 +0900

    register to pypi

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup (
+    name = 'github_listener',
+    version = '0.0.1',
+
+    author = 'taeguk',
+    author_email = 'xornrbboy@gmail.com',
+    
+    url = 'https://github.com/taeguk/github_listener',
+    license='MIT',
+    description = "The simple library that calls user's registered functions whenever github events happen. (ex, notification)",
+    long_description = long_description,
+    
+    packages=find_packages(),
+    include_package_data = True,
+
+    install_requires = ['requests', 'beautifulsoup4'],
+    
+    keywords = 'github notification',
+)
