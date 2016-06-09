dd9f735590ef10f4ddd51e27fad8188b5d6c2cf8
taeguk <xornrbboy@gmail.com>
2016-06-09 02:54:34 -0400

    fix #6. check validation of GithubAccount.

--- github_listener/github_api/github_api.py
+++ github_listener/github_api/github_api.py
@@ -4,6 +4,16 @@ class GithubAccount:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        if not self.is_valid():
+            raise ValueError('Account is invalid. Username or password is incorrect.')
+        
+    def is_valid(self):
+        import requests
+        r = requests.get('https://api.github.com/notifications', 
+            auth = (self.username, self.password))
+        if r.status_code == 401: # Not Authorized
+            return False
+        return True
 
 class GithubAPI:
     def __init__(self, account):
